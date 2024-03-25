457a3ed79734f37077d67fe9c09e0a30fcb378fd
kuhuyjr97 <kuhuyjr97>
2024-03-25 02:19:28 +0000

    asd

--- pages/ExchangeToken/jsobjects/auth/auth.js
+++ pages/ExchangeToken/jsobjects/auth/auth.js
@@ -9,7 +9,7 @@ export default {
 		try {
 			await gg_get_token.run()
 
-			const tokenInfo = gg_get_token.data
+			const tokenInfo =await gg_get_token.data
 			const idToken = tokenInfo.id_token;
 			const decodedToken = await jsonwebtoken.decode(idToken);
 			const email = decodedToken.email
@@ -18,18 +18,18 @@ export default {
 			const id = check_email_exist.data[0].id	
 			if (check_email_exist.data.length === 0 ){
 				await clearStore()
-				return await navigateTo("Login");
+				// return await navigateTo("Login");
 			}
 
-			await storeValue('token', tokenInfo);
-			await storeValue('user', decodedToken);
-			await add_token_to_db.run({id, idToken});
+			// await storeValue('token', tokenInfo);
+			// await storeValue('user', decodedToken);
+			// await add_token_to_db.run({id, idToken});
 
 			showAlert('Check ok','success')
-			await navigateTo("Home");
+			// await navigateTo("Home");
 		} catch(error) {
 			showAlert('You are not allow to login this page','error')
-			await navigateTo("Login");
+			// await navigateTo("Login");
 		}
 	},
 
