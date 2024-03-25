1409bcac1a5297a01d74a17c1cf4eb1377e64f86
kuhuyjr97 <kuhuyjr97>
2024-03-25 02:30:14 +0000

    as

--- pages/ExchangeToken/jsobjects/auth/auth.js
+++ pages/ExchangeToken/jsobjects/auth/auth.js
@@ -6,31 +6,27 @@ export default {
 			// return await navigateTo("Home");
 		}
 
-		try {
-			await gg_get_token.run()
+		await gg_get_token.run()
 
-			const tokenInfo =await gg_get_token.data
-			const idToken = tokenInfo.id_token;
-			const decodedToken = await jsonwebtoken.decode(idToken);
-			const email = decodedToken.email
-			await check_email_exist.run({email}).catch((error)=>{'email'+email+error,'error'})
+		const tokenInfo =await gg_get_token.data
+		const idToken = tokenInfo.id_token;
+		const decodedToken = await jsonwebtoken.decode(idToken);
+		const email = decodedToken.email
+		await check_email_exist.run({email}).catch((error)=>{'email'+email+error,'error'})
 
-			const id = check_email_exist.data[0].id	
-			if (check_email_exist.data.length === 0 ){
-				await clearStore()
-				// return await navigateTo("Login");
-			}
-
-			// await storeValue('token', tokenInfo);
-			// await storeValue('user', decodedToken);
-			// await add_token_to_db.run({id, idToken});
-
-			showAlert('Check ok','success')
-			// await navigateTo("Home");
-		} catch(error) {
-			showAlert('You are not allow to login this page'+error,'error')
-			// await navigateTo("Login");
+		const id = check_email_exist.data[0].id	
+		if (check_email_exist.data.length === 0 ){
+			await clearStore()
+			// return await navigateTo("Login");
 		}
+
+		// await storeValue('token', tokenInfo);
+		// await storeValue('user', decodedToken);
+		// await add_token_to_db.run({id, idToken});
+
+		showAlert('Check ok','success')
+		// await navigateTo("Home");
+
 	},
 
 
