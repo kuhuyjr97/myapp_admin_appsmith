efa19b3955362bf1458e7b87edf72dbcf313ec2f
kuhuyjr97 <kuhuyjr97>
2024-03-25 02:34:07 +0000

    asd

--- pages/ExchangeToken/jsobjects/auth/auth.js
+++ pages/ExchangeToken/jsobjects/auth/auth.js
@@ -5,27 +5,31 @@ export default {
 		if (tokenExist) {
 			// return await navigateTo("Home");
 		}
+		try{
+			await gg_get_token.run()
+			const tokenInfo =await gg_get_token.data
+			const idToken = tokenInfo.id_token;
+			// const decodedToken = await jsonwebtoken.decode(idToken);
+			// const email = decodedToken.email
+			// await check_email_exist.run({email}).catch((error)=>{'email'+email+error,'error'})
+			// 
+			// const id = check_email_exist.data[0].id	
+			// if (check_email_exist.data.length === 0 ){
+			// await clearStore()
+			// // return await navigateTo("Login");
+			// }
 
-		await gg_get_token.run()
+			await storeValue('token', idToken);
+			// await storeValue('user', decodedToken);
+			// await add_token_to_db.run({id, idToken});
 
-		const tokenInfo =await gg_get_token.data
-		const idToken = tokenInfo.id_token;
-		const decodedToken = await jsonwebtoken.decode(idToken);
-		const email = decodedToken.email
-		await check_email_exist.run({email}).catch((error)=>{'email'+email+error,'error'})
-
-		const id = check_email_exist.data[0].id	
-		if (check_email_exist.data.length === 0 ){
-			await clearStore()
-			// return await navigateTo("Login");
+			showAlert('Check ok','success')
+			// await navigateTo("Home");
+		}catch(error){
+			showAlert(error,'error')
 		}
 
-		// await storeValue('token', tokenInfo);
-		// await storeValue('user', decodedToken);
-		// await add_token_to_db.run({id, idToken});
 
-		showAlert('Check ok','success')
-		// await navigateTo("Home");
 
 	},
 
